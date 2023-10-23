83b1874ea500c40aafb555f550017170d17fa285
konekowo <70107937+konekowo@users.noreply.github.com>
2023-10-22 20:10:39 -0400

    start partial rewrite of SongSelectCarousel.cs

---
+++ Assets/BeatmapsDB.cs
@@ -0,0 +1,31 @@
+﻿public struct BeatmapsDB
+{
+    public string SID;
+    public string Title;
+    public string Artist;
+    public string Mapper;
+    public string ImgDir;
+    public string SongDir;
+    public string SongFileName;
+    public string Difficulty;
+    public string PreviewTime;
+    public string OsuFile;
+    public string SongFolder;
+    public string StarRating;
+
+    public void convert(string mapDiff)
+    {
+        string[] data = mapDiff.Split("📂");
+        SID = data[0];
+        Title = data[1];
+        Artist = data[2];
+        Mapper = data[3];
+        ImgDir = data[4];
+        SongDir = data[5];
+        SongFileName = data[6];
+        Difficulty = data[7];
+        PreviewTime = data[8];
+        OsuFile = data[9];
+        SongFolder = data[10];
+    }
+}
