4d6e7aa720613016b3180fc0eaa608d47ee0eaf0
konekowo <70107937+konekowo@users.noreply.github.com>
2023-11-01 13:04:20 -0400

    add something i shoulda added a long time ago

---
+++ Assets/GithubLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubLink : MonoBehaviour
+{
+    public void onClick()
+    {
+        Application.OpenURL("https://github.com/konekowo/kosu/");
+    }
+}
